77c4bad5f2bd91fd8d92da2f1ad0b59f0d9a3cbe
tbxavierrocha <tulio.fem@gmail.com>
2023-02-24 10:30:48 -0300

    Add project files.

---
+++ Data/SistemaTarefasDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data.Map;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data
+{
+    public class SistemaTarefasDBContext : DbContext
+    {
+        public SistemaTarefasDBContext(DbContextOptions<SistemaTarefasDBContext> options) 
+            : base(options) 
+        { 
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefa { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            modelbuilder.ApplyConfiguration(new UsuarioMap());
+            modelbuilder.ApplyConfiguration(new TarefaMap());
+           
+            base.OnModelCreating(modelbuilder);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+//https://www.youtube.com/watch?v=2TxePNK0kc8&list=PLU_KcnG1LJwXK6fPkM9HXuaDzfUVEeKkg&index=3
+
+using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Repisitorios;
+using SistemaDeTarefas.Repisitorios.Interfaces;
+
+namespace SistemaDeTarefas
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddEntityFrameworkSqlServer()
+                .AddDbContext<SistemaTarefasDBContext>(
+                    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase"))
+                );
+
+            builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
